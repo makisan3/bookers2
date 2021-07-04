50290b7b8b55b7735042705916e5d1d49390324b
makisan3 <nerimaki921@gmail.com>
2021-07-04 12:27:32 +0000

    [Finish3]

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,11 @@ class Book < ApplicationRecord
   validates :body, presence: true
   validates :body, length: { maximum: 200 }
 
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   get "/home/about" => "homes#about" , as: "home/about"
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:show, :edit, :update, :index]
 
 end
