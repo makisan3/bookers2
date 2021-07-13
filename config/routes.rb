8634a46b466e31a4d34f37faa467be1f77ac7fee
makisan3 <nerimaki921@gmail.com>
2021-07-13 14:11:12 +0000

    [Update7]

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
+    resources :reviews, only: [:index, :create]
   end
 
   resources :users, only: [:show, :edit, :update, :index]do
